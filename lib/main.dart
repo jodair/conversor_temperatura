e465698841e2def12c1ed74c7397d65601b4ae87
Jodair Neves <jodair.neves@gmail.com>
2019-05-11 12:00:48 -0300

    Alteração  de estilo adicionada

--- lib/main.dart
+++ lib/main.dart
@@ -26,10 +26,33 @@ class _HomeState extends State<Home>{
 
     Icon icon = Icon(Icons.wb_sunny, size: 120, color: Colors.yellow,);
 
+    TextStyle styleDecoration = TextStyle(color: Colors.blueAccent, fontSize: 20);
+    TextStyle styleField = TextStyle(color: Colors.blueAccent);
+
+    TextField tempCelsius = TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: "Temperatura em graus Celsius",
+        labelStyle: styleDecoration,
+      ),
+      textAlign: TextAlign.center,
+      style: styleField,
+    );
+    
+    TextField tempFahrenheit = TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: "Temperatura em graus Fahrenheit",
+        labelStyle: styleDecoration,
+      ),
+      textAlign: TextAlign.center,
+      style: styleField,
+    );
+
     Column column = Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        icon,
+        icon, tempCelsius, tempFahrenheit,
 
       ],
     );
